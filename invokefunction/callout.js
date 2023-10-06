11b7a692c5296e4bfcf581153de8193652b2b726
sumit <sumitmessi018@gmail.com>
2023-10-06 15:43:40 +0530

    first commit

---
+++ invokefunction/callout.js
@@ -0,0 +1,26 @@
+const hello = ()=>{
+    console.log("how are you where have been you since last two days")
+    return "hii"
+}
+function onePlusAvg(x ,y){
+    console.log("code is done")
+return Math.round(1 + (x + y)/2)
+}
+
+const sum = (p , q)=>{
+    return p + q
+}
+
+let a =2;
+let b = 4;
+let c = 8;
+   let v = hello();
+console.log(v)
+
+
+
+console.log("one plus average of a  and b is ", onePlusAvg(a , b) )
+
+console.log("one plus average of b  and c is ",onePlusAvg(b , c))
+console.log("one plus average of a  and c is ", onePlusAvg(a , c))
+console.log(sum(12 ,6))
